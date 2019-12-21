5be39daa91e3e567d1144e0b88d0efc17f38cce5
13166560446 <2256006378@qq.com>
2019-12-21 14:07:48 +0800

    商品详情

--- routes/web.php
+++ routes/web.php
@@ -33,4 +33,4 @@ Route::get('/weixin/menu','Weixin\WeixinController@createmenu');
 
 Route::get('/vote','VoteController@index');//微信投票
 //微商城
-Route::get('/goods/detail','Goods\IndexController@detail');//微信投票
+Route::get('/goods/detail','Goods\IndexController@detail');
